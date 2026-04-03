c6e571ce8cab3961f9287a31d911ba42812c2047
jamuna144 <jamunabaiju043@gmail.com>
2026-04-03 10:29:41 +0545

    After changes in vite

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
-});
+  esbuild: {
+    jsx: 'automatic', // ✅ Correct location
+    // loader: 'jsx', // Only if you must process .js files as JSX
+  }
+})
